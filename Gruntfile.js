98f0104f951b612305033a775b7937dbad9e149c
zaman27 <shimul271@gmail.com>
2014-12-03 18:31:18 -0800

    sharif

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
       options: {
         port: 7000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '192.168.191.17',
+        hostname: '127.0.0.1',
         livereload: 35729
       },
       livereload: {
