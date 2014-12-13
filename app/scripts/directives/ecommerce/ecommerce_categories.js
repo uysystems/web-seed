8c1bc38dc7dd82598a364b845a8fe9e3dafa1e35
programmer-zubayer <programmer.zubayer@gmail.com>
2014-12-13 14:50:51 +0600

    foo

--- app/scripts/directives/ecommerce/ecommerce_categories.js
+++ app/scripts/directives/ecommerce/ecommerce_categories.js
@@ -1,62 +1,29 @@
 'use strict';
 webApp.directive('ecommercecategories',['$rootScope','$http','$compile','$sce', function ($scope,$http,$compile,$sce) {
-	$scope.ecommerce_cat_loading = true;
-	
-	//tree generator
-	var treeGenerator = function(threaded) {
-		
-		if((threaded.length)>0){
-			var html = '<ul>';
-			angular.forEach(threaded,function(node,key) {
-				html += '<li>';
-				angular.forEach (node,function(threaded,type) {
-					if (type !== 'children') {
-						html +=  "<a data-ng-href='/#/shop/category_view/"+threaded.id+"'>"+threaded.title+'</a>'
-					} else {
-						if ((threaded.length)>0) {
-							html += treeGenerator(threaded);
-						}
-					}
-				})
-				html += '</li>';
-			})
-			html += '</ul>';
-			
-			return html;
-		}
-	}
+
 	
 	
 	return {
-		restrict: 'E',
+		restrict: 'EA',
 		transclude: true,
-		
-		controller : function(){
+		link : function(scope, element, attrs){
+			
+			$scope.ecommerce_cat_loading = true;
 			$http({
 				url		: sp['shop_category_tree'],
 				method 	: 'get',
-				cache 	: true,
+				cache 	: false,
 				headers : {'content-type': 'application/x-www-form-urlencoded'}
 			}).success(function(data){
 				$scope.ecommerce_cat_loading = false;
 				$scope.error_message = '';
-				$scope.categoryTree = treeGenerator(data.category_tree);
-						
+				$scope.categoryTreeData = data.category_tree;
 			}).error(function(status){
 				$scope.error_message = 'Network Error occured. Please reload page.';
 			});
-		},
-		
-		link : function(scope, element, atttributes){
-			scope.$watch('categoryTree',function(nv,ov){
-				if(nv != ov){
-					$scope.categoryTree = $compile(nv) (scope);
-					//console.log($scope.categoryTree)
-					$('.categoryTreeUl').html($scope.categoryTree)
-					//$scope.categoryTree = htmlString($scope.categoryTree);
-					//console.log($scope.categoryTree)
-				}
-			})
+			scope.$watch('categoryTreeData',function(nv,ov){
+				$scope.categoryTree = nv;
+			});
 		},
 		
 		templateUrl : 'views/directives/ecommerce/ecommerce_categories.html'
@@ -70,3 +37,29 @@ webApp.directive('ecommercecategories',['$rootScope','$http','$compile','$sce', 
 	
 	
   }]);
+
+//tree generator
+/*
+$scope.treeGenerator = function(threaded) {
+	if((threaded.length)>0){
+		var html = '<ul>';
+		$.each(threaded,function(key,node) {
+			html += '<li>';
+			$.each(node,function(type,threaded) {
+				if (type !== 'children') {
+					html +=  "<a data-ng-href='/#/shop/category_view/"+threaded.id+"'>"+threaded.title+'</a>'
+				} else {
+					if ((threaded.length)>0) {
+						html += $scope.treeGenerator(threaded);
+					}
+				}
+			})
+			html += '</li>';
+		})
+		html += '</ul>';
+		
+		return html;
+	}
+}
+*/
+//
